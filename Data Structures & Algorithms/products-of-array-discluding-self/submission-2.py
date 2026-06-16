3d622432911a206206461cd637a64517b0244395
Ahmet Sercan Kuru <139001176+S9-gif@users.noreply.github.com>
2026-06-17 00:43:14 +0300

    Add: products-of-array-discluding-self - submission-2

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-2.py
@@ -0,0 +1,66 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        
+
+        liste = [1] * len(nums)
+        # -->[0,0,0,0] şeklinde bir dizi oluşturmuş oldum işlemlerimi buraya ekliycem.
+        #liste[0] = diğer elemanlar çarğımı
+        i = 0
+
+        while i < len(nums):
+
+            for j in list(range(0, i)) + list(range(i+1, len(nums))):
+                
+                liste[i] *= nums[j]
+
+            i = i + 1
+
+        return liste
+
+
+                
+
+                
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        """
+PROBLEM:
+Verilen nums dizisi için her eleman için kendisi hariç diğer tüm sayıların çarpımını döndürücez
+Yani dizinin birinci elemanı için karşılık gelecek değer birinci eleman hariç diğer tüm elemanların 
+çarpımı.
+
+
+
+
+* i̇ki tane for döngüsüyle i. İndeks hariç diğer elemanlara ulaşabilirim fakat burdaki sorun
+Her j elemanının çarpımını birikimli olarak eklememiz gerektiği.
+
+
+*********ÇÖZÜM*********
+
+
+Topic'ler arasında prefix sum görünce konuyu araştırdım ve toplama işlemi için birikimli listelerde 
+sıfır listesinin üzerine eklemelerin yapıldığını gördüm.Bu problem çarpma olduğu için etkisiz eleman
+1 olacaktur bu nedenle [1,1,1,1] şeklinde bir liste oluşturdum sonra sınırı parçalamam gerekliydi
+bunun için de iki farklı sınırı liste halinde kullandım .Gerisi çarpımı yerleştirmekteydi.
+
+
+
+
+
+        """
